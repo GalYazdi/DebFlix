e8b8b0ea22981bfc9ea7ab6c6f1f3206f6154845
GalYazdi <galyazdi11@gmail.com>
2025-05-22 18:52:07 +0300

    initialize basic Fastify server

--- Backflix/src/index.ts
+++ Backflix/src/index.ts
@@ -1,2 +1,13 @@
 import fastify from "fastify";
 
+
+const app = fastify({ logger: true });
+
+app.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+  app.log.info(`Server listening at ${address}`);
+});
+

aa14293d964528bf38bea481d85446b7b758238c
GalYazdi <galyazdi11@gmail.com>
2025-05-22 18:03:53 +0300

    added function to take care of adding new movie to the movies arr

---
+++ Backflix/src/services/addMovie.ts
@@ -0,0 +1,23 @@
+type Movie = {
+  id: number;
+  title: string;
+  year: number;
+};
+
+let movies: Movie[] = [];
+
+export function addMovie(movie: Omit<Movie, "id">): Movie {
+  const existingMovie = movies.find(
+    (m) => m.title === movie.title && m.year === movie.year
+  );
+  if (existingMovie) {
+    throw new Error("Movie already exists");
+  }
+
+  const newMovie: Movie = {
+    ...movie,
+    id: movies.length + 1,
+  };
+  movies.push(newMovie);
+  return newMovie;
+}

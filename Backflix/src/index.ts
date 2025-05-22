4ca0a7949113270996cf5e5ba49fec04d369bed6
GalYazdi <galyazdi11@gmail.com>
2025-05-22 19:01:43 +0300

    merge main into movies-endpoint

--- Backflix/src/index.ts
+++ Backflix/src/index.ts
@@ -1,2 +1,15 @@
 import fastify from "fastify";
+import movieRoutes from "./routes/MovieRoutes";
+
+
+const app = fastify({ logger: true });
+app.register(movieRoutes, { prefix: "/movies" });
+
+app.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+  app.log.info(`Server listening at ${address}`);
+});
 

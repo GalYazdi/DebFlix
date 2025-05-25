d69edacb54dce9092ef0b25bdad5b42df94e5ff3
GalYazdi <galyazdi11@gmail.com>
2025-05-25 08:43:11 +0300

    Adds request logging middleware movie routes
    added hook as middleware in the movie routes to log details
    of incoming requests, including method, URL, headers, parameters,
    and query.

--- Backflix/src/routes/movieRoutes.ts
+++ Backflix/src/routes/movieRoutes.ts
@@ -1,14 +1,22 @@
 import { FastifyInstance } from "fastify";
-import { get } from "http";
 import {
   addMovieHandler,
   getMoviesHandler,
   getMovieByIdHandler,
   deleteMovieHandler,
 } from "../controllers/moviesController";
-import { getMovies } from "../services/moviesServices";
 
 export default async function movieRoutes(fastify: FastifyInstance) {
+  fastify.addHook("onRequest", async (request, reply) => {
+    console.log("New /movies request:");
+    console.log("Method:", request.method);
+    console.log("URL:", request.url);
+    console.log("Headers:", request.headers); 
+    console.log("Params:", request.params);
+    console.log("Query:", request.query); 
+
+  });
+
   fastify.post("/add", addMovieHandler);
   fastify.get("/getAll", getMoviesHandler);
   fastify.get("/get/:id", getMovieByIdHandler);

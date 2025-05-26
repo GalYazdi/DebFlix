9f1c42ced923da583749cc8ac61b09c9673fa918
GalYazdi <galyazdi11@gmail.com>
2025-05-26 09:12:50 +0300

    Adds request logging for DELETE method in actor routes

--- Backflix/src/routes/actorsRoutes.ts
+++ Backflix/src/routes/actorsRoutes.ts
@@ -8,6 +8,13 @@ import {
 } from "../controllers/actorsController";
 
 export default async function actorRoutes(fastify: FastifyInstance) {
+  fastify.addHook("onRequest", async (request, reply) => {
+    if (request.method === "DELETE") {
+      console.log("Method:", request.method);
+      console.log("URL:", request.url);
+      console.log("Query:", request.query);
+    }
+  });
   fastify.post("/add", addActorHandler);
   fastify.get("/getAll", getActorsHandler);
   fastify.get("/get/:id", getActorByIdHandler);

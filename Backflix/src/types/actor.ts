c67012bd94b408fd35a2b7bdf0b818c37099f28e
GalYazdi <galyazdi11@gmail.com>
2025-06-03 12:35:56 +0300

    removed id from interface

--- Backflix/src/types/actor.ts
+++ Backflix/src/types/actor.ts
@@ -2,7 +2,6 @@ import { BaseEntity } from "./base";
 import { Movie } from "./movie";
 
 export interface Actor extends BaseEntity {
-  id: string;
   name: string;
   age: number;
   movies: Movie[];

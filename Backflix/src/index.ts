86af28fcaa8e3311b2faa34aa0fd175095c921d1
GalYazdi <galyazdi11@gmail.com>
2025-06-03 17:27:01 +0300

    Merged all points to one branch and added cors

--- Backflix/src/index.ts
+++ Backflix/src/index.ts
@@ -1,18 +1,24 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import movieRoutes from "./routes/moviesRoute";
 import categoriesRoute from "./routes/categoriesRoute";
 import actorRoutes from "./routes/actorsRoutes";
 
-const app = fastify({ logger: true });
-app.register(movieRoutes, { prefix: "/movies" });
+const start = async () => {
+  const app = fastify({ logger: true });
+  await app.register(cors, { origin: "*" });
 
-app.register(categoriesRoute, { prefix: "/categories" });
-app.register(actorRoutes, { prefix: "/actors" });
+  app.register(movieRoutes, { prefix: "/movies" });
+  app.register(categoriesRoute, { prefix: "/categories" });
+  app.register(actorRoutes, { prefix: "/actors" });
 
-app.listen({ port: 3000 }, (err, address) => {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-  app.log.info(`Server listening at ${address}`);
-});
+  app.listen({ port: 3000 }, (err, address) => {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+    app.log.info(`Server listening at ${address}`);
+  });
+};
+
+start();
--- Backflix/src/utils/handleRequest.ts
+++ Backflix/src/utils/handleRequest.ts
@@ -9,7 +9,7 @@ export const handleRequest = async (
   try {
     return reply.status(statusCode).send(await callback());
   } catch (error) {
-    const message = (error as any).message ?? "Unknown Error";
+    const message = error instanceof Error ? error.message : "Unknown error";
     return reply
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .send({ error: message });
